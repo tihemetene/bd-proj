86b664164d1cf63dc234b233fc5a7014b48cbf41
Tiago Henrique <tihemetene@gmail.com>
2021-06-10 00:08:07 -0300

    criação do repo

---
+++ backend/src/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "batyr.db.elephantsql.com/trvilbdk",
+  USER: "trvilbdk",
+  PASSWORD: "3unc4U8rGklwxUJEWcCQSTj0y0zGG8-h",
+  DB: "trvilbdk",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
